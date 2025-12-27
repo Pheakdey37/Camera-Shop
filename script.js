af0bdb2c77033f065480057647856288b41b29a3
Pheakdey37 <kdeyp870@gmail.com>
2025-12-27 11:26:14 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-document.getElementById("loginForm").addEventListener("submit", function (event) {
-  event.preventDefault(); // បញ្ឈប់ form មិនអោយ reload
+function changeImage(imageSrc) {
+  document.getElementById('mainImg').src = imageSrc;
+}
+const imageInput = window.localStorage.getItem('fileImage');
+if (imageInput !== null) {
+console.log('true');
+document.getElementById('mainImg').src = imageInput;
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  // ពិនិត្យបញ្ចូល
-  if (email === "Pheakdey" && password === "12345") {
-    alert("✅ Login successful!");
-    window.location.href = '/assiginment.javascript/back-end/index1.html'; // ឲ្យទៅទំព័រ dashboard
-  } else {
-    alert("❌ Incorrect email or password!");
-  }
-});
+}
